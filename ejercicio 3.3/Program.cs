a27aefe177795cd5fa5f080160acddea73fd0b47
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 00:15:20 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio 3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace lapices
+{
+    class lapices
+    {
+        static void Main()
+        {
+            double cantidad, precio, total;
+            Console.WriteLine("ingrese la cantidad de lapices");
+            cantidad = double.Parse(Console.ReadLine());
+            if (cantidad >= 1000) ;
+            {
+                precio = 85;
+                total = (cantidad * precio);
+            }
+            if (cantidad < 1000) ;
+            {
+                precio = 90;
+                total = (cantidad * precio);
+            }
+            Console.WriteLine($"el costo total de los lapices es de {total}");
+        }
+    }
+}
